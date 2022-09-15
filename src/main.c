359a4914ae1099e412787bb2122871074001bb5e
vde-vasc <vde-vasc@student.42.rio>
2022-09-15 12:58:29 -0300

    New Tileset

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 01:27:10 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/09/15 07:37:20 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/09/15 11:50:23 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,15 @@ int	keye_hook(int keycode)
 
 {
 	if (keycode == 13)
-		ft_printf("BOM DIA LETICIA!!!\n");
+		ft_printf("W\n");
 	if (keycode == 0)
-		ft_printf("ESPERO QUE VOCE TENHA UM EXCELENTE DIA HOJE!\n");
+		ft_printf("A\n");
 	if (keycode == 1)
-		ft_printf("POIS VOCE MERECE BASTANTE\n");
+		ft_printf("S\n");
 	if (keycode == 2)
-		ft_printf("PORQUE VAI SOFRER NO LABORATORIO HAHAHA beijos!\n");
+		ft_printf("D\n");
 	if (keycode == 3)
-		ft_printf("❤️  ❤️  ❤️  ❤️  ❤️  ❤️  ❤️ \n");
+		ft_printf("❤️ \n");
 	return (0);
 }
 
@@ -37,7 +37,7 @@ int	main(void)
 
 	start.server = mlx_init();
 	start.window = mlx_new_window(start.server, 352, 352, "Test");
-	img.sprite = mlx_xpm_file_to_image(start.server, VICTOR, &img.width, &img.height);
+	img.sprite = mlx_xpm_file_to_image(start.server, MADEIRA, &img.width, &img.height);
 	while (++i * 32 <= 320)
 		mlx_put_image_to_window(start.server, start.window, img.sprite, i * 32, 0);
 	i = -1;
