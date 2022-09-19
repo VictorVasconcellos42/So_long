ffbff9d11691d4466dd76134b967e33dd77acd2a
vde-vasc <vde-vasc@student.42.rio>
2022-09-19 19:19:43 -0300

    Add events hook keyboard and X buttom

--- src/main.c
+++ src/main.c
@@ -6,49 +6,55 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 01:27:10 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/09/15 11:50:23 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/09/19 18:28:11 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-int	keye_hook(int keycode)
+int	key_X_hook(void)
 
 {
-	if (keycode == 13)
-		ft_printf("W\n");
-	if (keycode == 0)
-		ft_printf("A\n");
-	if (keycode == 1)
-		ft_printf("S\n");
-	if (keycode == 2)
-		ft_printf("D\n");
-	if (keycode == 3)
-		ft_printf("❤️ \n");
+	exit(0);
+	return (0);
+}
+
+int	key_hook(int keycode, t_begin *core)
+
+{
+	if (keycode == 53)
+	{
+		mlx_destroy_image(core->server, core->sprite);
+		mlx_destroy_window(core->server, core->window);
+		exit(0);
+	}
 	return (0);
 }
 
 int	main(void)
 
 {
-	t_begin		start;
-	t_sprite	img;
+	t_begin		core;
 	int			i = -1;
 
-	start.server = mlx_init();
-	start.window = mlx_new_window(start.server, 352, 352, "Test");
-	img.sprite = mlx_xpm_file_to_image(start.server, MADEIRA, &img.width, &img.height);
+	core.server = mlx_init();
+	core.window = mlx_new_window(core.server, 352, 352, "Test");
+	core.sprite = mlx_xpm_file_to_image(core.server, MADEIRA, &core.width, &core.height);
 	while (++i * 32 <= 320)
-		mlx_put_image_to_window(start.server, start.window, img.sprite, i * 32, 0);
+		mlx_put_image_to_window(core.server, core.window, core.sprite, i * 32, 0);
 	i = -1;
 	while (++i * 32 <= 320)
- 		mlx_put_image_to_window(start.server, start.window, img.sprite, 0, i * 32);
+ 		mlx_put_image_to_window(core.server, core.window, core.sprite, 0, i * 32);
 	i = -1;
 	while (++i * 32 <= 320)
- 		mlx_put_image_to_window(start.server, start.window, img.sprite, 320, i * 32);
+ 		mlx_put_image_to_window(core.server, core.window, core.sprite, 320, i * 32);
 	i = -1;
 	while (++i * 32 <= 320)
- 		mlx_put_image_to_window(start.server, start.window, img.sprite, i * 32, 320);
-	mlx_loop(start.server);
+ 		mlx_put_image_to_window(core.server, core.window, core.sprite, i * 32, 320);
+	core.sprite = mlx_xpm_file_to_image(core.server, VICTOR, &core.width, &core.height);
+	mlx_put_image_to_window(core.server, core.window, core.sprite, 32, 32);
+	mlx_key_hook(core.window, key_hook, &core);
+	mlx_hook(core.window, 17, 0, key_X_hook, &core);
+	mlx_loop(core.server);
 	return (0);
 }
