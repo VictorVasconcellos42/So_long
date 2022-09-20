357f04e70a57e82fb589c5c23a35cc41fe374322
vde-vasc <vde-vasc@student.42.rio>
2022-09-20 03:40:11 -0300

    Start my game project

---
+++ src/start.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   start.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 03:06:11 by vde-vasc          #+#    #+#             */
+/*   Updated: 2022/09/20 03:28:28 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	main(int argc, char *argv[])
+
+{
+	t_server	mlx;
+
+	if	(argc != 2)
+		return (ft_printf("Invalid format, try ./So_long <map.ber>"));
+}
