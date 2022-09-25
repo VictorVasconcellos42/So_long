fd55be17642cc9429f89c1273448a527489d184f
vde-vasc <vde-vasc@student.42.rio>
2022-09-25 09:52:40 -0300

    main program for my game

---
+++ src/so_long.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 20:37:38 by vde-vasc          #+#    #+#             */
+/*   Updated: 2022/09/25 00:16:50 by vde-vasc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	close_X(void)
+
+{
+	exit(0);
+}
+
+int	close_window(int keycode, t_config *config)
+
+{
+	if (keycode == ESC)
+	{
+		mlx_destroy_window(config->server, config->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int	main(int argc, char *argv[])
+
+{
+	t_config config;
+
+	if (argc != 2)
+		return (ft_printf("Error: ./so_long maps/<map>.ber only!"));
+	config.phase = map_generator(&config, argv[1]);
+	if (!config.phase)
+		return (0);
+	start_game(&config);
+	append_map(&config);
+	mlx_key_hook(config.win, keys_game, &config);
+	mlx_hook(config.win, 17, 2, close_X, &config);
+	mlx_loop_hook(config.server, &append_map, &config);
+	mlx_loop(config.server);
+}
