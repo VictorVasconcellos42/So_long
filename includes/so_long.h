0e64b927266fe3c81efcd031447f9282619c4f21
vde-vasc <vde-vasc@student.42.rio>
2022-09-25 09:55:53 -0300

    modifidy and add new types for my game

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 05:32:07 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/09/23 14:57:29 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/09/25 00:23:13 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,32 @@
 # define VICTOR "images/victor.xpm"
 # define WALLS "images/madeira.xpm"
 # define BRAHMA "images/brahma.xpm"
+# define FLOOR "images/walls.xpm"
+# define EXIT "images/exit.xpm"
+# define W_PLAYER "images/d.xpm"
+# define A_PLAYER "images/a.xpm"
+# define S_PLAYER "images/victor.xpm"
+# define D_PLAYER "images/w.xpm"
 
-# define S_SPRITE 32
+# define SPRITE 32
+
+enum	e_keys
+
+{
+	W_KEY = 13,
+	A_KEY = 0,
+	S_KEY = 1,
+	D_KEY = 2,
+	ESC = 53
+};
 
 // My struct for player in my game:
 
 typedef struct	s_avatar
 
 {
-	int		pos_x;
-	int		pos_y;
+	int		pos_w;
+	int		pos_h;
 }	t_avatar;
 
 // My struct's struct for my game:
@@ -43,18 +59,38 @@ typedef struct	s_config
 {
 	void	*server;
 	void	*win;
-	void	*sprite;
+	void	*player;
+	void	*floor;
+	void	*walls;
+	void	*brahma;
+	void	*exit;
 
-	int		width;
-	int		height;
-	int		win_w;
-	int		win_h;
+
+	int		end;
+	int		count_brahmas;
 	int		phase_columns;
 	int		phase_rows;
 
-	char	**phase;
-	char	**copy_phase;
+	char		**phase;
+	char		**copy_phase;
+
+	t_avatar	avatar;
 
 }	t_config;
+
+int		possible_move(int wid, int hei, t_config *config);
+void    avatar_img(t_config *config, char *sprite);
+void    avatar_move(t_config *config, int wid, int hei);
+void    moviments(int keycode, t_config *config);
+int		keys_game(int keycode, t_config *config);
+int 	start_game(t_config *config);
+void    *input_image(t_config *config, char *path_img);
+void    insert(t_config *config, void *image, int wid, int hei);
+void    set_phase(t_config *config, int wid, int hei);
+int		append_map(t_config *config);
+void    append_map_size(t_config *config);
+char    **reader(int fd);
+char    **map_generator(t_config *config, char *path_file);
+int		close_window(int keycode, t_config *config);
 
 #endif
