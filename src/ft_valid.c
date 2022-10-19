210bf810c66eb6b828bb95dd25f43679b7823d52
vde-vasc <vde-vasc@student.42.rio>
2022-10-19 00:26:33 -0300

    New rules to valid my map created

--- src/ft_valid.c
+++ src/ft_valid.c
@@ -3,11 +3,75 @@
 /*                                                        :::      ::::::::   */
 /*   ft_valid.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vde-vasc <vde-vasc@student.42.rio>         +#+  +:+       +#+        */
+/*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:14:19 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/10/17 11:14:20 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/10/19 00:23:14 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/so_long.h"
 
+static int	has_plus_newline(char *string)
+
+{
+	int	i;
+
+	i = -1;
+	while (string[++i])
+		if (string[i] == '\n' && string[i + 1] == '\n')
+			return (error_msg("Wrong Format!!!"));
+	return (0);
+}
+
+static int	valid_map_format(t_config *config)
+
+{	
+	const int	width_len = ft_strlen(config->copy_phase[0]);
+	int			i;
+
+	i = 1;
+	{
+	while (config->copy_phase[i])
+		if (width_len != ft_strlen(config->phase_rows[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+static int	valid_walls(t_config	*config, int i, int j, int count)
+
+{
+	while (config->copy_phase[i])
+	{
+		if (config->copy_phase[i + 1] == NULL || i == 0)
+		{
+			j = -1;
+			while (config->copy_phase[i][++j])
+				if (config->copy_phase[i][j] != '1')
+					return (1);
+		}
+		j = ft_strlen(config->copy_phase[i]) - 1;
+		if (config->copy_phase[i][0] == '1')
+			count++;
+		if (config->copy_phase[i][j] == '1')
+			count++;
+		if (count != 2)
+			return (1);
+		else
+			count = 0;
+		i++;
+	}
+	return (0);
+}
+
+int	ft_valid(t_config	*config)
+
+{
+	if (valid_map_format(config) == 1)
+		return (error_msg("Map isn't a rectangle!!"));
+	else if (valid_walls(config, 0, 0, 0) == 1)
+		return (error_msg("Map isn't surrounded by walls"));
+	return (0);
+}
