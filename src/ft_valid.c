2ca587ce5ae7feb66f3bb2702267b5fa459251b4
vde-vasc <vde-vasc@student.42.rio>
2022-10-24 19:17:11 -0300

    valid coin confirmed

--- src/ft_valid.c
+++ src/ft_valid.c
@@ -6,7 +6,7 @@
 /*   By: vde-vasc <vde-vasc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 11:14:19 by vde-vasc          #+#    #+#             */
-/*   Updated: 2022/10/24 18:39:20 by vde-vasc         ###   ########.fr       */
+/*   Updated: 2022/10/24 18:48:24 by vde-vasc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -105,5 +105,6 @@ int	ft_valid(t_config *config)
 		return (error_msg("Map isn't surrounded by walls"));
 	else if (valid_quantiy(config, 0, 0) == 1)
 		return (error_msg("Map Invalid, Too many or too few characters"));
+	config->count_brahmas = config->coin_amount;
 	return (0);
 }
